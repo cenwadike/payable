fa7559d9b58e2e13271781fcc29794824e6e94ad
cenwadike <chinedunwadike04@gmail.com>
2024-09-21 15:07:03 +0100

    create invoice added

--- programs/payable/src/lib.rs
+++ programs/payable/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::{Mint, Token};
 
 declare_id!("8rrFrtdFK3x8NBFEvaqznHg9Q9Tf2ij6bHEv2FCHotgJ");
 
@@ -6,46 +7,210 @@ declare_id!("8rrFrtdFK3x8NBFEvaqznHg9Q9Tf2ij6bHEv2FCHotgJ");
 pub mod payable {
     use super::*;
 
-    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
-        emit!(Initialized {});
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        counter.invoice_idx_counter = 0;
 
+        emit!(Initialized {});
+        Ok(())
+    }
+
+    pub fn create_invoice(
+        ctx: Context<CreateInvoice>, 
+        amount: u128, 
+        recurrent: bool, 
+        number_of_recurrent_payment: u128, 
+        recurrent_payment_interval: u128,
+        cancel_period: i64
+    ) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        let invoice = &mut ctx.accounts.invoice;
+        let clock = Clock::get()?;
+
+        // check creator is not payer
+        require_keys_neq!(ctx.accounts.signer.key(), ctx.accounts.payer_1.key());
+        require_keys_neq!(ctx.accounts.signer.key(), ctx.accounts.payer_2.key());
+        require_keys_neq!(ctx.accounts.signer.key(), ctx.accounts.payer_3.key());
+        require_keys_neq!(ctx.accounts.signer.key(), ctx.accounts.payer_3.key());
+        require_keys_neq!(ctx.accounts.signer.key(), ctx.accounts.payer_4.key());
+        require_keys_neq!(ctx.accounts.signer.key(), ctx.accounts.payer_5.key());
+        
+        // create payers vec
+        let mut payers = vec![];
+        payers.push(ctx.accounts.payer_1.key());
+        payers.push(ctx.accounts.payer_2.key());
+        payers.push(ctx.accounts.payer_3.key());
+        payers.push(ctx.accounts.payer_4.key());
+        payers.push(ctx.accounts.payer_5.key());
+
+        let invoice_idx = counter.invoice_idx_counter;
+        let creator = ctx.accounts.signer.key();
+        let valid_token = ctx.accounts.valid_token_mint.key();
+
+        // create invoice 
+        invoice.invoice_idx = invoice_idx;
+        invoice.amount = amount;
+        invoice.cancel_period = clock.unix_timestamp + cancel_period;
+        invoice.creator = ctx.accounts.signer.key();
+        invoice.payers.append(&mut payers);
+        invoice.recurrent = recurrent;
+        invoice.number_of_recurrent_payment = number_of_recurrent_payment;
+        invoice.recurrent_payment_interval = recurrent_payment_interval;
+        invoice.valid_payment_token = ctx.accounts.valid_token_mint.key();
+        invoice.last_withdrawal = clock.unix_timestamp;
+        invoice.status = 0; // locked
+
+        // update counter
+        counter.invoice_idx_counter += 1;
+
+        // emit event
+        emit!(InvoiceCreated {
+            invoice_idx,
+            creator,
+            payers,
+            valid_token,
+            amount
+        });
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(
+        init,
+        payer = signer,
+        space = 8 + Counter::LEN,
+        seeds = [b"counter"], 
+        bump
+    )]
+    pub counter: Account<'info, Counter>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    // account holding the contract binary
+    pub system_program: Program<'info, System>,
+}
 
+#[derive(Accounts)]
+pub struct CreateInvoice<'info> {
+    #[account(mut, seeds = [b"counter"], bump)]
+    pub counter: Account<'info, Counter>,
+    #[account(
+        init_if_needed,
+        payer = signer,
+        space = 8 + Invoice::LEN,
+        seeds = [
+            b"invoice", 
+            signer.key().as_ref(), 
+            payer_1.key().as_ref(), 
+            payer_2.key().as_ref(), 
+            payer_3.key().as_ref(), 
+            payer_4.key().as_ref(), 
+            payer_5.key().as_ref()
+        ], 
+        bump
+    )]
+    pub invoice: Account<'info, Invoice>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    /// CHECK: safe
+    /// must provide payer one
+    #[account(mut)]
+    pub payer_1: AccountInfo<'info>,
+    
+    // use random address as default value
+    /// CHECK: safe
+    #[account(mut)]
+    pub payer_2: AccountInfo<'info>,
+    /// CHECK: safe
+    #[account(mut)]
+    pub payer_3: AccountInfo<'info>,
+    /// CHECK: safe
+    #[account(mut)]
+    pub payer_4: AccountInfo<'info>,
+    /// CHECK: safe
+    #[account(mut)]
+    pub payer_5: AccountInfo<'info>,
+
+    #[account(mut)]
+    pub valid_token_mint: Account<'info, Mint>,
+
+    pub token_program: Program<'info, Token>,
+    // account holding the contract binary
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct Counter {
+    pub invoice_idx_counter: u128,
+}
 
 #[account]
 pub struct Invoice {
     pub invoice_idx: u128,
     pub amount: u128,
-    pub critical_period: u128,
+    pub cancel_period: i64,
     pub creator: Pubkey,
     pub payers: Vec<Pubkey>, // max 5 payers on a single invoice
     pub recurrent: bool,
     pub number_of_recurrent_payment: u128,
     pub recurrent_payment_interval: u128,
     pub valid_payment_token: Pubkey,
-    pub last_withdrawal: u128,
+    pub last_withdrawal: i64,
     pub status: u8, // 0 = locked, 1 = unlocked, 2 = completed
+}
+
+impl Counter {
+    pub const LEN: usize =
+        (1 + 16);
 }
 
 impl Invoice {
     pub const LEN: usize =
         (1 + 16)  +         // invoice_idx
         (1 + 16)  +         // amount
-        (1 + 16)  +         // critical period
+        (1 + 8)  +          // critical period
         (1 + 32)  +         // creator
         (1 + (5 * 32)) +    // payers
         (1 + 1)  +          // recurrent
         (1 + 16)  +         // number of recurrent payment
         (1 + 16)  +         // recurrent payment interval
         (1 + 32)  +         // valid token
-        (1 + 16)  +         // last withdrawal
+        (1 + 8)  +         // last withdrawal
         (1 + 1);            // critical period 
 }
 
 #[event]
 pub struct Initialized {}
+
+#[event]
+pub struct InvoiceCreated {
+    pub invoice_idx: u128,
+    pub creator: Pubkey,
+    pub payers: Vec<Pubkey>,
+    pub valid_token: Pubkey,
+    pub amount: u128
+}
+
+#[event]
+pub struct CancelPeriodOver {
+    pub invoice_idx: u128
+}
+
+#[event]
+pub struct InvoiceWithdrawal {
+    pub invoice_idx: u128,
+    pub creator: Pubkey,
+    pub payers: Vec<Pubkey>,
+    pub valid_token: Pubkey,
+    pub amount: u128
+}
+
+#[event]
+pub struct InvoiceCompleted {
+    pub invoice_idx: u128,
+    pub creator: Pubkey,
+    pub payers: Vec<Pubkey>,
+    pub valid_token: Pubkey,
+}
--- tests/payable.ts
+++ tests/payable.ts
@@ -1,16 +1,177 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Payable } from "../target/types/payable";
+import { Connection, Keypair, LAMPORTS_PER_SOL, PublicKey, Signer, SystemProgram } from "@solana/web3.js";
+import { createMint, getOrCreateAssociatedTokenAccount, mintTo } from "@solana/spl-token";
 
-describe("payable", () => {
+const TestProgram = async () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+  
+  const connection = new Connection(
+    'http://127.0.0.1:8899', "confirmed"
+  )
 
   const program = anchor.workspace.Payable as Program<Payable>;
+  
+  const admin = Keypair.generate();
+  const adminSig: Signer = {
+    publicKey: admin.publicKey,
+    secretKey: admin.secretKey
+  }
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
-});
+  const payee = Keypair.generate();
+  const payeeSig: Signer = {
+    publicKey: payee.publicKey,
+    secretKey: payee.secretKey
+  }
+
+  const payer1 = Keypair.generate();
+  // const payer1Sig: Signer = {
+  //   publicKey: payer1.publicKey,
+  //   secretKey: payer1.secretKey
+  // }
+
+  const payer2 = Keypair.generate();
+  const payer3 = Keypair.generate();
+  const payer4 = Keypair.generate();
+  const payer5 = Keypair.generate();
+
+  const mint = Keypair.generate();
+
+  const [counterPDA, _a] = PublicKey.findProgramAddressSync(
+    [
+      anchor.utils.bytes.utf8.encode("counter"),
+    ],
+    program.programId
+  )
+
+  const [invoicePDA, _f] = PublicKey.findProgramAddressSync(
+    [
+      anchor.utils.bytes.utf8.encode("invoice"),
+      payee.publicKey.toBuffer(),
+      payer1.publicKey.toBuffer(),
+      payer2.publicKey.toBuffer(),
+      payer3.publicKey.toBuffer(),
+      payer4.publicKey.toBuffer(),
+      payer5.publicKey.toBuffer(),
+    ],
+    program.programId
+  )
+
+  await provider.connection.confirmTransaction(
+    await provider.connection.requestAirdrop(
+      provider.wallet.publicKey,
+      10 * LAMPORTS_PER_SOL
+    ),
+    "confirmed"
+  );
+  await provider.connection.confirmTransaction(
+    await provider.connection.requestAirdrop(
+      program.programId,
+      10 * LAMPORTS_PER_SOL
+    ),
+    "confirmed"
+  );
+  await provider.connection.confirmTransaction(
+    await provider.connection.requestAirdrop(
+      admin.publicKey,
+      10 * LAMPORTS_PER_SOL
+    ),
+    "confirmed"
+  );
+  await provider.connection.confirmTransaction(
+    await provider.connection.requestAirdrop(
+      payee.publicKey,
+      10 * LAMPORTS_PER_SOL
+    ),
+    "confirmed"
+  );
+  await provider.connection.confirmTransaction(
+    await provider.connection.requestAirdrop(
+      payer1.publicKey,
+      10 * LAMPORTS_PER_SOL
+    ),
+    "confirmed"
+  );
+  await provider.connection.confirmTransaction(
+    await provider.connection.requestAirdrop(
+      mint.publicKey,
+      10 * LAMPORTS_PER_SOL
+    ),
+    "confirmed"
+  );
+  
+  // create valid token
+  const token = await createMint(
+    connection,
+    mint,
+    mint.publicKey,
+    null,
+    9
+  );
+  const tokenAccount = await getOrCreateAssociatedTokenAccount(
+    connection,
+    mint,
+    token,
+    mint.publicKey
+  )
+  await mintTo(
+    connection,
+    mint,
+    token,
+    tokenAccount.address,
+    mint,
+    1000 * 9 // mint 1000
+  )
+
+  console.log("-----------------------ADMIN ADDRESS: ", admin.publicKey.toBase58());
+  console.log("-----------------------PAYEE ADDRESS: ", payee.publicKey.toBase58());
+  console.log("-----------------------VALID TOKEN MINT: ", token.toBase58());
+  console.log("-----------------------COUNTER PDA ADDRESS: ", counterPDA.toBase58());
+
+  // console.log("-----------------------STARTING INITIALIZATION--------------------------");
+  // const initTx = await program.methods.initialize().accounts(
+  //   {
+  //     counter: counterPDA,
+  //     signer: admin.publicKey,
+  //     systemProgram: SystemProgram.programId
+  //   }
+  // ).signers([adminSig]).rpc();
+  // console.log("-----------------------INITIALIZATION SUCCESSFUL:", initTx.toString());
+
+  console.log("-----------------------STARTING INVOICE CREATION--------------------------");
+
+  const createInvoiceTx = await program.methods.createInvoice(
+    new anchor.BN(1),
+    false,
+    new anchor.BN(1),
+    new anchor.BN(0),
+    new anchor.BN(1)
+  ).accounts({
+    counter: counterPDA,
+    invoice: invoicePDA,
+    signer: payee.publicKey,
+    payer1: payer1.publicKey,
+    payer2: payer2.publicKey,
+    payer3: payer3.publicKey,
+    payer4: payer4.publicKey,
+    payer5: payer5.publicKey,
+    validTokenMint: token,
+    systemProgram: SystemProgram.programId
+  }).signers([payeeSig]).rpc()
+  console.log("-----------------------INVOICE CREATION SUCCESSFUL:", createInvoiceTx.toString());
+};
+
+const runTest = async () => {
+  try {
+    await TestProgram();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+}
+
+runTest()
